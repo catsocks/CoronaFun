bdb4b9b9712fa1764fb0bee65603ad1a0e7002d9
catsocks <3389801+catsocks@users.noreply.github.com>
2018-11-19 08:58:55 -0300

    Add a green rectangle

--- main.lua
+++ main.lua
@@ -4,38 +4,45 @@ print("Hello, World!")
 -- Make the background white.
 display.setDefault("background", 255, 255, 255)
 
--- Create a retangle in the center of the screen, sized the 15th of the screen
--- width.
-local rect = display.newRect(
-    display.contentCenterX,
+-- Create a rectangle on each side of the screen.
+local rectA = display.newRect(
+    display.contentCenterX - display.contentCenterX / 4,
     display.contentCenterY,
     display.contentWidth / 15,
     display.contentWidth / 15
 )
 
--- Make it blue.
-rect.fill = {0, 0.5, 1}
+local rectB = display.newRect(
+    display.contentCenterX + display.contentCenterX / 4,
+    display.contentCenterY,
+    display.contentWidth / 15,
+    display.contentWidth / 15
+)
+
+-- Make one blue and the other green.
+rectA.fill = {0, 0.5, 1}
+rectB.fill = {0, 1, 0.5}
 
 -- Define a handler for key events.
 local function onKeyEvent(event)
-    local x = rect.x
-    local y = rect.y
+    local x = rectA.x
+    local y = rectA.y
 
     -- Ignore if the key is being released.
     if (event.phase == "down") then
         if (event.keyName == "up") then
-            y = y - rect.height
+            y = y - rectA.height
         elseif (event.keyName == "down") then
-            y = y + rect.height
+            y = y + rectA.height
         elseif (event.keyName == "left") then
-            x = x - rect.width
+            x = x - rectA.width
         elseif (event.keyName == "right") then
-            x = x + rect.width
+            x = x + rectA.width
         end
     end
 
     -- Transition the rectangle to a new position.
-    transition.to(rect, {time=100, transition=easing.inOutSine, y=y, x=x})
+    transition.to(rectA, {time=100, transition=easing.inOutSine, y=y, x=x})
  
     -- This function should not override the OS's own key handler.
     return false
